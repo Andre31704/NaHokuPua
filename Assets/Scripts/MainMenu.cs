11e263e0de002287dece3c4a402c2d186b0fb37e
bloaco <68760604+bloaco@users.noreply.github.com>
2023-11-17 15:57:09 -1000

    setup for cutscenes 2

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame ()
     {
-        SceneManager.LoadScene("NaPuaHoku");
+        SceneManager.LoadScene("Beginning Cutscene");
     }
 
     public void QuitGame () 
